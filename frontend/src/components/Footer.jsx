add27a1d9d610aa83706f5eee5afa3f6e2cf750d
erik465 <ehalasz06@gmail.com>
2022-08-05 11:30:38 +0200

    slight changes

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,34 @@ import React from 'react'
 function Footer() {
   return (
     <footer>
+      <div className="footer--main">
+        <div className="footer--links">
+          <a href="">Support me</a>
+          <a href="">Information</a>
+          <a href="">GitHub Repo</a>
+       </div>
+        <div className="footer--contact">
+          <a href="mailto:ehalasz06@gmail.com">Work email : ehalasz06@gmail.com</a>
+          <p>Erik Halasz</p>
+        </div>
+      </div>
       
+      <hr />
+
+      <div className="footer--social">
+        <a href="https://github.com/erik465" target="_blank">
+          <p>GitHub</p>
+          <img src="github.png" alt="" />
+        </a>
+        <a href="https://twitter.com/ErikHalasz1" target="_blank">
+          <p>Twitter</p>
+          <img src="twitter.png" alt="" />
+        </a>
+        <a href="https://www.reddit.com/user/Able_Choice1015/" target="_blank">
+          <p>Reddit</p>
+          <img src="reddit.png" alt="" />
+        </a>
+      </div>
     </footer>
   )
 }
